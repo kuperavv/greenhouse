61d07b04df13c23a7ce9a27628c5ff9d4e4dc9bc
kuperavv <vince.kuperavage@gmail.com>
2024-02-17 12:49:19 -0700

    simple server

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const PORT = 3001;
+const app = express();
+
+const data = {
+    "temperature": 80
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.json(data)
+})
+
+app.post("/data", (req, res) => {
+    data.temperature = Number(req.body.temperature)
+    res.sendStatus(200)
+})
+
+app.listen(PORT, "10.0.0.164", () => {
+    console.log(`listening on port ${PORT}`)
+})
