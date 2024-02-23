62530a643333376f9a4074475f0af0a5a7d10e13
kuperavv <vince.kuperavage@gmail.com>
2024-02-22 21:33:14 -0700

    server running

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
+// local ip: 10.0.0.164
 const PORT = 3001;
 const app = express();
 
-const data = {
-    "temperature": 80
-}
+let data = {};
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.json(data)
+    res.json(data);
 })
 
 app.post("/data", (req, res) => {
-    data.temperature = Number(req.body.temperature)
-    res.sendStatus(200)
+    if (req.headers['x-secret-auth'] === 'AWESOME_GREENHOUSE') {
+        data = req.body;
+    }
+    res.sendStatus(200);
 })
+
+
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
